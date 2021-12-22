78cf1b61f14d5c00af33633b0c4bf2ce1f7972e5
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-12-22 17:11:41 +0100

    feat(command): création d'une commande permettant de faire un git status

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,38 @@
   windows_subsystem = "windows"
 )]
 
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+#[derive(PartialEq, Default, Clone, Debug)]
+struct Commit {
+    hash: String,
+    message: String,
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![git_status])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn git_status(repository: String) -> String {
+    set_repository(&repository);
+
+    let command = Command::new("git")
+        .arg("status")
+        .output()
+        .expect("Failed to execute git");
+
+    String::from_utf8_lossy(&command.stdout).to_string()
+}
+
+fn set_repository(repository: &String)
+{
+    let root = Path::new(&repository);
+    assert!(env::set_current_dir(&root).is_ok());
+    println!("Repository: {}", root.display());
 }

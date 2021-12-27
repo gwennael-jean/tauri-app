5f0aa79d45bdd3e616d52d44758a04becc076697
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-12-27 22:47:30 +0100

    feat(font-awesome): importation de la librairie

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import App from './App.svelte';
 
+import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+
 const app = new App({
 	target: document.body,
 	props: {

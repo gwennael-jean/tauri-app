e28c711fb159ec5a9fcb6b056b8b0351f1855562
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-12-27 22:59:40 +0100

    feat(action): préparation des différents boutons d'action

--- src/components/ActionBar.svelte
+++ src/components/ActionBar.svelte
@@ -3,9 +3,50 @@
     import { user } from "../stores/user";
 </script>
 
-<main class="flex flex-row bg-secondary p-1 border-b border-primary">
-    {get(user).firstname}
-    {get(user).lastname}
+<main class="flex justify-between flex-row bg-secondary p-1 border-b border-primary">
+    <ul class="flex flex-row">
+        <li>
+            <a href="#" class="px-1" title="Open Repository">
+                <i class="fas fa-folder-open"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="px-1" title="Fetch">
+                <i class="fas fa-sync"></i>
+            </a>
+        </li>
+    </ul>
+    <ul class="flex flex-row">
+        <li>
+            <a href="#" class="px-1" title="Pull">
+                <i class="fas fa-download"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="px-1" title="Push">
+                <i class="fas fa-upload"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="px-1" title="Branch">
+                <i class="fas fa-code-branch"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="px-1" title="Stash">
+                <i class="fas fa-box"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="px-1" title="Pop">
+                <i class="fas fa-box-open"></i>
+            </a>
+        </li>
+    </ul>
+    <div>
+        {get(user).firstname}
+        {get(user).lastname}
+    </div>
 </main>
 
 <style>
